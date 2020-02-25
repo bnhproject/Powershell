9d8c957186530db92873fc4dd36896fb41ea067e
bnhproject <60041053+bnhproject@users.noreply.github.com>
2020-02-24 19:09:43 -0600

    Add files via upload

---
+++ prompts.ps1
@@ -0,0 +1,18 @@
+﻿# Brandon Hunt 00186124
+try{
+Do {
+$task = (Read-Host "Enter a number from 1-4 or enter 5 to quit.");
+ 
+ switch ($task) {
+    1 {Get-ChildItem $PSScriptRoot -Filter *.log | Out-File -FilePath $PSScriptRoot\DailyLog.txt -Append; break} #Finds .log files in the same directory as the script and appends them to DailyLog.txt
+       2 {Get-ChildItem $PSScriptRoot | Sort-Object Name | Format-Table | Out-File $PSScriptRoot\C916contents.txt -Append ; break}# Lists all files in the same directory as the script, formats to tabular format in alphabetical order ascending and appends to C916contents.txt
+          3 {Get-Counter -Counter "\Processor(*)\% Processor Time", "\Memory\Committed Bytes" -SampleInterval 5 -MaxSamples 4 ; break} # Gets 4 samples at 5 second intervals of processor time and memory usage.
+             4 {Get-Process| Sort-Object CPU -Descending | Out-GridView ; break} #Shows running processes in Gridview sorted by cpu time from greatest to least.
+                5 {"Goodbye."; break}# Exits switch and ptints Goodbye
+                   default {"Invalid Input!"; break}# prints invalid input
+                   }
+                   } until ($task -eq 5)
+}
+catch [System.OutOfMemoryException] {
+    Write-Host "A system out of memory exception occured."# Catches system out of memory error
+    }
---
+++ restore.ps1
@@ -0,0 +1,45 @@
+﻿# Brandon Hunt 00186124
+
+
+try{
+#Create OU named finance and import financepersonnel.csv directly into the new OU
+New-ADOrganizationalUnit -Name finance -ProtectedFromAccidentalDeletion $false
+
+$NewAD = Import-Csv $PSScriptRoot\financepersonnel.csv
+$Path = "OU=finance,DC=ucertify,DC=com"
+
+foreach ($ADUser in $NewAD)
+{
+$First = $ADUser.First_name
+$Last = $ADUser.Last_name
+$Name =  $ADUser.samAccount
+$Display = $ADUser.First_Name + " " + $ADUser.Last_name
+$Zip = $ADUser.PostalCode
+$OfficePhone = $ADUser.OfficePhone
+$MobilePhone = $ADUser.MobilePhone
+
+New-ADUser -Name $Name -GivenName $First -Surname $Last -DisplayName $Display -PostalCode $Zip -OfficePhone $OfficePhone -MobilePhone $MobilePhone -Path $Path
+}
+#Create new database and table and import client data
+Import-Module sqlps -DisableNameChecking -Force
+$servername = ".\UCERTIFY3"
+$srv = New-Object Microsoft.SqlServer.Management.Smo.Server -ArgumentList $servername
+$databasename = "ClientDB"
+$db = New-Object Microsoft.SqlServer.Management.Smo.Database -ArgumentList $srv, $databasename
+$db.Create()
+
+Invoke-Sqlcmd -ServerInstance .\UCERTIFY3 -Database ClientDB -InputFile $PSScriptRoot\contacts.sql
+
+$table = "dbo.Client_A_Contacts"
+$db = "ClientDB"
+
+Import-CSV $PSScriptRoot\NewClientData.csv | ForEach-Object {Invoke-Sqlcmd `
+-Database ClientDB -ServerInstance .\UCERTIFY3 -Query "insert into $table (first_name,last_name,city,county,zip,officePhone,mobilePhone) Values`
+('$($_.first_name)','$($_.last_name)','$($_.city)','$($_.county)','$($_.zip)','$($_.officePhone)','$($_.mobilePhone)')"
+}
+}
+#Catch statement for system out of memory exception
+catch [System.OutOfMemoryException] {
+    WriteHost "A system out of memory exception has occured."
+}
+
